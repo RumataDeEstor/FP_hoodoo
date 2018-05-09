d79eb3ca0f73f162cf87223cd3c9cae0da55fc7d
Alina Piletskaya <rumatadeestor@gmail.com>
2018-05-10 02:02:28 +0300

    added Two

--- ChurchNum.hs
+++ ChurchNum.hs
@@ -1,20 +1,21 @@
-import Base(flip', constant)
+import Base (flip', constant, apply, compose, sasai, identity)
 
 -- zero*
 zero :: t1 -> t -> t
 -- zero f x = x
 zero = flip' constant
+
 -- one*
 one :: (t1 -> t) -> t1 -> t
 --one f x = f x
 one = ($)
 
 -- two*
-
-
+two :: (t -> t) -> t -> t
+--two f x = f (f x)
+two = sasai compose identity 
 
 -- inc*
-
 
 
 -- dec
