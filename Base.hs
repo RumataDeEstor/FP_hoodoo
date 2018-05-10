ce049fa8a11733295b1aff6c5bbcfa695ffe0a19
Straj <Mr.Straj@yandex.ru>
2018-05-11 02:06:56 +0300

    Refactor. Added inc function

--- Base.hs
+++ Base.hs
@@ -17,9 +17,9 @@ compose :: (t2 -> t1) -> (t3 -> t2) -> t3 -> t1
 compose f g x = f(g(x))
 
 -- S - sasai
-sasai :: (x -> g -> f) -> (x -> g) -> x -> f
+sasai :: (t2 -> t1 -> t) -> (t2 -> t1) -> t2 -> t
 sasai f g x = f x (g x)
 
 -- flip
-flip' :: (x -> y -> f) -> y -> x -> f
+flip' :: (t2 -> t1 -> t) -> t1 -> t2 -> t
 flip' f x y = f y x
