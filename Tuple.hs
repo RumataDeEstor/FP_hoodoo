1cc66b63d6f4ab7f13968fbf001849c7453703c1
Straj <Mr.Straj@yandex.ru>
2018-06-05 23:26:09 +0300

    Added first function

--- Tuple.hs
+++ Tuple.hs
@@ -1,11 +1,13 @@
+import Base (constant, apply, flip')
+
 -- tuple
 tuple :: t -> t1 -> (t -> t1 -> t2) -> t2
 tuple a b f = f a b
 
 -- first*
-
-
-
+first :: ((t2 -> t1 -> t2) -> t) -> t
+-- first t = t constant   
+first = flip' apply constant
 -- second
 
 
