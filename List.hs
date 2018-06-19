50ff7bcc66196e9eeb425fdd867fa2fd61be3fee
Straj <Mr.Straj@yandex.ru>
2018-06-20 01:13:31 +0300

    Added foldr function

--- List.hs
+++ List.hs
@@ -2,7 +2,7 @@
 
 module List where
 
-import Prelude (Show, Integer)
+import Prelude (Show, Integer, (+))
 
 -- list type definition
 data List a
@@ -10,7 +10,7 @@ data List a
   | Cons a (List a)
   deriving(Show)
 
-
-
-
-
+-- Foldr
+foldr :: List a -> (a -> a -> a) -> a -> a
+foldr Empty _ x = x
+foldr (Cons h t) f x = f h (foldr t f x)
